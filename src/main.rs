d57fde67cf1d8befaf4c633146909419016b3a43
Brian Caswell <bcaswell@microsoft.com>
2024-05-07 15:17:00 -0400

    update dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,68 @@
-#[macro_use]
-extern crate clap;
-extern crate retry;
+use clap::{Parser, ValueEnum};
+use retry::{
+    delay::{jitter, Fibonacci, Fixed, NoDelay},
+    retry_with_index, OperationResult,
+};
+use std::{process::Command, time::Duration};
 
-use clap::{App, AppSettings, Arg};
-use retry::delay::{jitter, Fibonacci, Fixed, NoDelay};
-use retry::OperationResult;
-use std::error::Error;
-use std::process::Command;
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("error: {0}")]
+    Error(String),
+}
 
-arg_enum! {
-    #[derive(PartialEq, Debug)]
-    pub enum Methods {
-        Fibonacci,
-        Fixed,
-        NoDelay,
+#[derive(Parser)]
+#[command(
+    author,
+    version,
+    propagate_version = true,
+    disable_help_subcommand = true
+)]
+struct Args {
+    #[clap(long, default_value = "3")]
+    retries: usize,
+    #[clap(long, default_value = "10")]
+    duration: u64,
+    #[clap(long, default_value = "fibonacci")]
+    method: Method,
+    #[clap(required = true)]
+    command: Vec<String>,
+}
+
+#[derive(PartialEq, Debug, ValueEnum, Clone, Copy)]
+pub enum Method {
+    Fibonacci,
+    Fixed,
+    NoDelay,
+}
+
+fn retry_time(
+    method: Method,
+    retries: usize,
+    duration: Duration,
+) -> Box<dyn Iterator<Item = Duration>> {
+    match method {
+        Method::Fibonacci => Box::new(Fibonacci::from(duration).map(jitter).take(retries)),
+        Method::Fixed => Box::new(Fixed::from(duration).map(jitter).take(retries)),
+        Method::NoDelay => Box::new(NoDelay.map(jitter).take(retries)),
     }
 }
 
-fn run() -> Result<(), Box<dyn Error>> {
-    let args = App::new("retry")
-        .about(env!("CARGO_PKG_DESCRIPTION"))
-        .version(env!("CARGO_PKG_VERSION"))
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .args(&[
-            Arg::with_name("retries")
-                .long("retries")
-                .takes_value(true)
-                .help("specify maximum number of times to retry")
-                .default_value("3"),
-            Arg::with_name("duration")
-                .long("duration")
-                .takes_value(true)
-                .help("duration (specified in tenths of a second)")
-                .default_value("10"),
-            Arg::with_name("method")
-                .long("method")
-                .takes_value(true)
-                .possible_values(&Methods::variants())
-                .default_value("Fibonacci")
-                .case_insensitive(true)
-                .help("retry methods"),
-        ])
-        .setting(AppSettings::AllowExternalSubcommands)
-        .get_matches();
+fn main() -> Result<(), Error> {
+    let Args {
+        retries,
+        duration,
+        method,
+        mut command,
+    } = Args::parse();
+    let duration = Duration::from_millis(duration.saturating_mul(100));
 
-    let retries = value_t!(args.value_of("retries"), usize)?;
-    let duration = value_t!(args.value_of("duration"), u64)? * 100;
-    let method = value_t!(args.value_of("method"), Methods)?;
+    let mut cmd = Command::new(command.remove(0));
+    if !command.is_empty() {
+        cmd.args(command);
+    }
 
-    let mut command = match args.subcommand {
-        Some(x) => {
-            let mut cmd = Command::new(x.name);
-            for arg in x
-                .matches
-                .values_of("")
-                .map_or(Vec::new(), Iterator::collect)
-            {
-                cmd.arg(arg);
-            }
-            cmd
-        }
-        None => {
-            return Err(From::from("no command provided"));
-        }
-    };
-
-    let func = |current_try: u64| match command.status() {
+    let func = |current_try: u64| match cmd.status() {
         Ok(status) => {
             if status.success() {
                 OperationResult::Ok(())
@@ -73,35 +70,11 @@ fn run() -> Result<(), Box<dyn Error>> {
                 if current_try <= retries as u64 {
                     eprintln!("failed, retrying...");
                 }
-                OperationResult::Retry(format!("continued to fail after {} retries", retries))
+                OperationResult::Retry(format!("continued to fail after {retries} retries"))
             }
         }
-        Err(fatal) => OperationResult::Err(format!("unable to execute: {:?}", fatal)),
+        Err(fatal) => OperationResult::Err(format!("unable to execute: {fatal:?}")),
     };
 
-    let res = match method {
-        Methods::Fibonacci => retry::retry_with_index(
-            Fibonacci::from_millis(duration).map(jitter).take(retries),
-            func,
-        ),
-        Methods::Fixed => {
-            retry::retry_with_index(Fixed::from_millis(duration).map(jitter).take(retries), func)
-        }
-        Methods::NoDelay => retry::retry_with_index(NoDelay.take(retries), func),
-    };
-
-    match res {
-        Ok(_) => Ok(()),
-        Err(err) => match err {
-            retry::Error::Internal(error) => Err(From::from(error)),
-            retry::Error::Operation { error, .. } => Err(From::from(error)),
-        },
-    }
-}
-
-fn main() {
-    if let Err(err) = run() {
-        eprintln!("retry failed: {}", err);
-        std::process::exit(1);
-    }
+    retry_with_index(retry_time(method, retries, duration), func).map_err(|e| Error::Error(e.error))
 }
